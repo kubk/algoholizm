812370914efa22a10e70bc1536f0dd745f4bdddf
Gorbachev Egor <7gorbachevm@gmail.com>
2022-06-03 18:08:18 +0300

    Parser combinators - parse HTML-like structure

---
+++ src/parsing/parser-combinators.test.ts
@@ -0,0 +1,154 @@
+import {
+  allExcept,
+  any,
+  defer,
+  NullablePattern,
+  oneOrMore,
+  optional,
+  sequence,
+  text,
+} from './parser-combinators';
+
+const clearAndJoinArrayIntoString = (array: string[]) =>
+  array
+    .flat()
+    .filter((value) => value !== null)
+    .join('');
+
+test('text', () => {
+  expect(text('1')('abc', 0)).toStrictEqual(null);
+  expect(text('a')('abc', 0)).toStrictEqual({ result: 'a', endPosition: 1 });
+  expect(text('a')('abc', 1)).toStrictEqual(null);
+});
+
+test('optional', () => {
+  expect(optional(text('1'))('abc', 0)).toStrictEqual({ result: null, endPosition: 0 });
+  expect(optional(text('a'))('abc', 0)).toStrictEqual({ result: 'a', endPosition: 1 });
+});
+
+test('any - letters', () => {
+  const aOrBOrC = any([text('a'), text('b'), text('c')]);
+  expect(aOrBOrC('a', 0)).toBeTruthy();
+  expect(aOrBOrC('d', 0)).toBeFalsy();
+});
+
+test('any - digit', () => {
+  const anyDigit = any([
+    text('0'),
+    text('1'),
+    text('2'),
+    text('3'),
+    text('4'),
+    text('5'),
+    text('6'),
+    text('7'),
+    text('8'),
+    text('9'),
+  ]);
+  expect(anyDigit('11', 0)).toStrictEqual({ result: '1', endPosition: 1 });
+  expect(anyDigit('12', 1)).toStrictEqual({ result: '2', endPosition: 2 });
+  expect(anyDigit('Phone +1234', 8)).toStrictEqual({ result: '2', endPosition: 9 });
+});
+
+test('sequence - abc', () => {
+  const aThenB = sequence([text('abc'), text('def')]);
+
+  expect(aThenB('abcdef', 0)).toStrictEqual({ result: ['abc', 'def'], endPosition: 6 });
+  expect(aThenB('abcde7', 0)).toBeFalsy();
+});
+
+test('oneOrMore', () => {
+  const anyDigit = any([
+    text('0'),
+    text('1'),
+    text('2'),
+    text('3'),
+    text('4'),
+    text('5'),
+    text('6'),
+    text('7'),
+    text('8'),
+    text('9'),
+  ]);
+  const number = oneOrMore(anyDigit, clearAndJoinArrayIntoString);
+  expect(number('1235', 0)!.result).toStrictEqual('1235');
+  expect(number('Phone +1234', 7)).toStrictEqual({ result: '1234', endPosition: 11 });
+  expect(number('Phone +1234', 6)).toStrictEqual(null);
+});
+
+test('integration - PHP union type parser', () => {
+  const flattenAndClear = (array: string[]) => array.flat().filter((value) => value !== null);
+
+  const optionalWhitespace = optional(oneOrMore(text(' '), () => null));
+  // prettier-ignore
+  const primitive = any([
+    text('bool'),
+    text('null'),
+    text('int'),
+    text('string'),
+  ]);
+  // prettier-ignore
+  const unionType: NullablePattern<string[]> = sequence(
+    [
+      primitive,
+      optional(sequence([
+        optionalWhitespace,
+        text('|', () => null),
+        optionalWhitespace,
+        optional(defer(() => unionType))
+      ], flattenAndClear)),
+    ],
+    flattenAndClear
+  );
+
+  // Table-driven tests
+  const table = [
+    { input: 'string|string|string', expected: ['string', 'string', 'string'] },
+    {
+      input: 'string| string | string| string  |    string',
+      expected: ['string', 'string', 'string', 'string', 'string'],
+    },
+    { input: 'int|int|int', expected: ['int', 'int', 'int'] },
+    { input: 'null|null|null', expected: ['null', 'null', 'null'] },
+    { input: 'bool|bool|bool', expected: ['bool', 'bool', 'bool'] },
+    { input: 'string|bool', expected: ['string', 'bool'] },
+    { input: 'bool|string', expected: ['bool', 'string'] },
+    { input: 'int|bool|string|null', expected: ['int', 'bool', 'string', 'null'] },
+  ];
+
+  table.forEach(({ input, expected }) => {
+    expect(unionType(input, 0)!.result).toStrictEqual(expected);
+  });
+});
+
+test('integration - simple HTML parser', () => {
+  const tagName = any([text('div'), text('p'), text('h1'), text('span')]);
+  const anyLettersAbc = oneOrMore(
+    any([text('a'), text('b'), text('c')]),
+    clearAndJoinArrayIntoString
+  );
+
+  // prettier-ignore
+  const html: NullablePattern<string[]> = sequence(
+    [
+      text('<'),
+      tagName,
+      text('>'),
+      oneOrMore(
+        any([anyLettersAbc, defer(() => html) as any]),
+        (array) => array.filter((value) => value !== null),
+      ),
+      text('</'),
+      tagName,
+      text('>'),
+    ],
+    (value) => ({
+      tag: value[1],
+      children: value[3],
+    })
+  );
+
+  const ast = html('<div><h1>aaa</h1><p>bb</p><div><span>c</span></div></div>', 0)!.result;
+
+  expect(ast).toMatchSnapshot()
+});
---
+++ src/parsing/parser-combinators.ts
@@ -0,0 +1,97 @@
+type PatternResultType = number | string | null | PatternResultType[];
+
+type PatternResult<T extends PatternResultType> = { result: T; endPosition: number };
+type NullablePatternResult<T extends PatternResultType> = PatternResult<T> | null;
+
+export type NullablePattern<T extends PatternResultType> = (
+  match: string,
+  startPosition: number
+) => NullablePatternResult<T>;
+
+export const text = (
+  match: string,
+  transform: (value: string) => any = (value) => value,
+): NullablePattern<string> => {
+  return (text, startPosition) => {
+    const index = text.substring(startPosition).indexOf(match);
+    if (index === 0) {
+      return { result: transform(match), endPosition: startPosition + index + match.length };
+    }
+    return null;
+  };
+}
+
+export const oneOrMore = <T extends PatternResultType>(
+  pattern: NullablePattern<T>,
+  transform: (value: T[]) => any = (value) => value
+): NullablePattern<T[]> => {
+  const newPattern: any = sequence([pattern, optional(defer(() => newPattern))], transform);
+  return newPattern;
+};
+
+export const optional = <T extends PatternResultType>(
+  pattern: NullablePattern<T>
+): NullablePattern<T> => {
+  return (text, startPosition) => {
+    return (
+      pattern(text, startPosition) ||
+      ({ endPosition: startPosition, result: null } as PatternResult<T>)
+    );
+  };
+};
+
+export const allExcept = <T extends PatternResultType>(
+  can: NullablePattern<T>,
+  canNot: NullablePattern<T>
+): NullablePattern<T> => {
+  return (text, startPosition) => {
+    if (!canNot(text, startPosition) && can(text, startPosition)) {
+      return can(text, startPosition);
+    }
+    return null;
+  };
+};
+
+export const any = <T extends PatternResultType>(
+  patterns: Array<NullablePattern<T>>
+): NullablePattern<T> => {
+  return (text, startPosition) => {
+    for (const pattern of patterns) {
+      const result = pattern(text, startPosition);
+      if (result) {
+        return result;
+      }
+    }
+    return null;
+  };
+};
+
+export const sequence = <T extends PatternResultType>(
+  patterns: Array<NullablePattern<T | T[]>>,
+  transform: (value: T[]) => any = (value) => value
+): NullablePattern<T[]> => {
+  return (text, startPosition) => {
+    const finalResult: NullablePatternResult<T[]> = { result: [], endPosition: startPosition };
+
+    for (const pattern of patterns) {
+      const patternResult = pattern(text, finalResult.endPosition);
+      if (patternResult) {
+        // @ts-ignore
+        finalResult.result.push(patternResult.result);
+        finalResult.endPosition = patternResult.endPosition;
+      } else {
+        return null;
+      }
+    }
+
+    return { ...finalResult, result: transform(finalResult.result) };
+  };
+};
+
+export const defer = <T extends PatternResultType>(
+  getPattern: () => NullablePattern<T>
+): NullablePattern<T> => {
+  return (text, startPosition) => {
+    return getPattern()(text, startPosition);
+  };
+};
