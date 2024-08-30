944e63d2c310a8d019a9420c6c68c1fc1784cc8e
Gorbachev Egor <7gorbachevm@gmail.com>
2024-08-30 11:36:22 +0700

    Optimize find-judge

--- src/graph/find-judge.ts
+++ src/graph/find-judge.ts
@@ -1,22 +1,29 @@
 export function findJudge(n: number, trust: number[][]): number {
-  const map: Record<number, number[]> = {};
-  const notJudges = new Set<number>()
+  if (trust.length === 0) return 1;
+
+  const fromToMap: Record<number, number[]> = {};
+  const toFromMap: Record<number, number[]> = {};
 
   for (let i = 0; i < trust.length; i++) {
     const connection = trust[i];
     const [from, to] = connection;
-    if (!map[to]) {
-      map[to] = [];
+
+    if (!toFromMap[to]) {
+      toFromMap[to] = [];
     }
-    notJudges.add(from);
-    map[to].push(from);
+    toFromMap[to].push(from);
+
+    if (!fromToMap[from]) {
+      fromToMap[from] = [];
+    }
+    fromToMap[from].push(to);
   }
 
-  const maybeJudges = Object.keys(map);
+  const maybeJudges = Object.keys(toFromMap);
 
   for (let i = 0; i < maybeJudges.length; i++) {
     const maybeJudge = parseInt(maybeJudges[i]);
-    if (map[maybeJudge]?.length === n - 1 && !notJudges.has(maybeJudge)) {
+    if (toFromMap[maybeJudge]?.length === n - 1 && !fromToMap[maybeJudge]) {
       return maybeJudge;
     }
   }
